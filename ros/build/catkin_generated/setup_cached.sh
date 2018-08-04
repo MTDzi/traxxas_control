0be5810cc9f4a2d824d6c9e0becc906b671341bb
maciej.dziubinski <maciej.dziubinskii@dcx.bz>
2018-08-04 21:26:08 +0200

    First (roslaunch launch/traxxas... works)

---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/md/git-projects/traxxas_control/ros/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/md/git-projects/traxxas_control/ros/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.0/lib64"
+export PWD="/home/md/git-projects/traxxas_control/ros/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/md/git-projects/traxxas_control/ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/md/git-projects/traxxas_control/ros/src:$ROS_PACKAGE_PATH"
