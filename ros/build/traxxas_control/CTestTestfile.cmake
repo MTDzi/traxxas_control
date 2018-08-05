d992bc6172bedd82ead81daa9cf426101053ba4b
MTDzi <ponadto@gmail.com>
2018-08-05 09:29:36 +0000

    Removed the ros/build directory

--- ros/build/traxxas_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/md/git-projects/traxxas_control/ros/src/traxxas_control
-# Build directory: /home/md/git-projects/traxxas_control/ros/build/traxxas_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ run.sh
@@ -0,0 +1,16 @@
+# In case roslaunch fails
+
+rosrun rosserial_python serial_node.py /dev/ttyACM0
+
+rosrun topic_tools transform /joy /servo std_msgs/Int16 'int(180*(m.axes[0]+1)/2)'
+rosrun topic_tools transform /joy /esc std_msgs/Int16 'int(105*m.axes[3])'
+
+rosrun joy joy_node
+
+
+# SD card
+/media/ubuntu/3C52-B9FD
+
+
+# ZED
+/usr/local/zed/tools
