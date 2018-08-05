b232cc64cb60aed904cf839a6746ce7523ddf924
MTDzi <ponadto@gmail.com>
2018-08-05 15:51:28 +0000

    ZED is working (although I needed to convert Image to CV_image)

--- ros/src/traxxas_control/scripts/traxxas_controller.py
+++ ros/src/traxxas_control/scripts/traxxas_controller.py
@@ -1,56 +1,73 @@
 #!/usr/bin/env python
 
 from math import asin, degrees, sqrt
+import numpy as np
+import cv2
+
 import rospy
 from std_msgs.msg import Int16
-from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Joy, Image
+from cv_bridge import CvBridge, CvBridgeError
 
 
-THROTTLE_MAXREVERSE = 1000
-THROTTLE_MAXFORWARD = 2000
-THROTTLE_IDLE = 1500
+class JoyController:
+
+    def __init__(self):
+        self.joy_msg = None
+        self.zed_depth_msg = None
+
+        self.bridge = CvBridge()
+	self.image_pub = rospy.Publisher("image_topic", Image)
+
+        self.pub_servo = rospy.Publisher('/servo', Int16, queue_size=1)
+        self.pub_esc = rospy.Publisher('/esc', Int16, queue_size=1)
+
+        rospy.Subscriber('/joy', Joy, self._joy_cb)
+        rospy.Subscriber('/zed/depth/depth_registered', Image, self._zed_depth_cb)
+
+        rospy.init_node('traxxas_controler')
+
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(50)
+        start_time = 0
+
+        while not rospy.is_shutdown():
+            elapsed = rospy.Time.now().to_sec() - start_time
+
+            if self.zed_depth_msg is not None:
+                ok_values = ~np.isnan(self.zed_depth_msg) & np.isfinite(self.zed_depth_msg)
+                rospy.logwarn('{}'.format(self.zed_depth_msg[ok_values].mean()))
+
+            if self.joy_msg is not None:
+                axes = self.joy_msg.axes
+
+                angle = int(90 + degrees(asin(axes[0])))
+                throt = int(102*np.sign(axes[3])*sqrt(abs(axes[3])))
+
+                self.pub_servo.publish(angle)
+                self.pub_esc.publish(throt)
 
 
-joy = None
 
+    # Callbacks
 
-def sgn(x):
-    return 0 if x == 0 else x / abs(x)
+    def _joy_cb(self, msg):
+        self.joy_msg = msg
 
-
-def joy_cb(msg):
-    global joy
-    joy = msg
-
-
-def loop():
-    pub_servo = rospy.Publisher('/servo', Int16, queue_size=1)
-    pub_esc = rospy.Publisher('/esc', Int16, queue_size=1)
-
-    rospy.Subscriber('/joy', Joy, joy_cb)
-
-    rospy.init_node('traxxas_controler')
-    rate = rospy.Rate(50)
-    start_time = 0
-
-    while not rospy.is_shutdown():
-        elapsed = rospy.Time.now().to_sec() - start_time
-
-        if joy is not None:
-            axes = joy.axes
-
-            angle = int(90 + degrees(asin(axes[0])))
-            throt = int(102*sgn(axes[3])*sqrt(abs(axes[3])))
-
-            pub_servo.publish(angle)
-            pub_esc.publish(throt)
-
-        rate.sleep()
-
+    def _zed_depth_cb(self, msg):
+        try:
+           cv_image = self.bridge.imgmsg_to_cv2(msg, 'passthrough')
+        except CvBridgeError as e:
+           print(e)
+ 
+        self.zed_depth_msg = cv_image
+ 
 
 
 if __name__ == '__main__':
     try:
         JoyController()
     except rospy.ROSInterruptException:
-        pass
+        rospy.logerr('JoyController failed')
