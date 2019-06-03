1b686d2b707844fbf638bbd9ae8b38eb3a069b01
MTDzi <mtdziubinski@gmail.com>
2019-06-03 17:39:02 +0000

    Added callibration of the game pad

--- ros/src/traxxas_control/scripts/traxxas_controller.py
+++ ros/src/traxxas_control/scripts/traxxas_controller.py
@@ -2,6 +2,7 @@
 
 from math import asin, degrees, sqrt
 import numpy as np
+from random import normalvariate
 #import cv2
 
 import rospy
@@ -13,49 +14,58 @@ from sensor_msgs.msg import Joy, Image
 class JoyController:
 
     def __init__(self):
-        self.joy_msg = None
-        self.zed_depth_msg = None
-        self.time_after_pub = None
-
-        #self.bridge = CvBridge()
-	#self.image_pub = rospy.Publisher("image_topic", Image)
-
         self.pub_servo = rospy.Publisher('/servo', Int16, queue_size=1)
         self.pub_esc = rospy.Publisher('/esc', Int16, queue_size=1)
 
+        self.servo_neutral = 1479
+        self.esc_neutral = 1500
+
+        self.angle_mult = 600 #80 # 600
+        self.throt_mult = 200 # 80 # 200
+
         rospy.Subscriber('/joy', Joy, self._joy_cb, queue_size=1)
-        #rospy.Subscriber('/zed/depth/depth_registered', Image, self._zed_depth_cb)
 
         rospy.init_node('traxxas_controler')
-
-
 
 
     # Callbacks
 
     def _joy_cb(self, msg):
-        self.joy_msg = msg
+        angle_mult_inc = msg.buttons[4]
+        angle_mult_dec = msg.buttons[6]
+        throt_mult_inc = msg.buttons[5]
+        throt_mult_dec = msg.buttons[7]
 
-        axes = self.joy_msg.axes
+        if angle_mult_inc:
+            self.angle_mult += 1
+            rospy.logwarn('self.angle_mult = {}'.format(self.angle_mult))
+        if angle_mult_dec:
+            self.angle_mult -= 1
+            rospy.logwarn('self.angle_mult = {}'.format(self.angle_mult))
 
-        angle = axes[0]
-        throt = axes[3]
-        angle = 1500 - int(505*angle)
-        throt = 1500 + int(120*throt)
+        if throt_mult_inc:
+            self.throt_mult += 1
+            rospy.logwarn('self.throt_mult = {}'.format(self.throt_mult))
+        if throt_mult_dec:
+            self.throt_mult -= 1
+            rospy.logwarn('self.throt_mult = {}'.format(self.throt_mult))
+
+        servo_neutral_shift = msg.axes[4]
+        esc_neutral_shift = msg.axes[5]
+        if servo_neutral_shift:
+            self.servo_neutral -= servo_neutral_shift
+            rospy.logwarn('self.servo_neutral = {}'.format(self.servo_neutral))
+        if esc_neutral_shift:
+            self.esc_neutral -= esc_neutral_shift
+            rospy.logwarn('self.esc_neutral = {}'.format(self.esc_neutral))
+
+        angle = msg.axes[0]
+        throt = msg.axes[3]
+        angle = self.servo_neutral - int(self.angle_mult*angle)
+        throt = self.esc_neutral + int(self.throt_mult*throt)
 
         self.pub_servo.publish(angle)
         self.pub_esc.publish(throt)
-
-    def _zed_depth_cb(self, msg):
-        try:
-           cv_image = self.bridge.imgmsg_to_cv2(msg, 'passthrough')
-        except CvBridgeError as e:
-           print(e)
- 
-        self.zed_depth_msg = cv_image
-
-    def _time_cb(self, msg):
-        self.time_after_pub = msg.data.to_sec()
 
 
 if __name__ == '__main__':
